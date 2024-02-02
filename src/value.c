0c35b926095ab1e5ec34599e2fcab431981283df
treyvon <Eugenewhitaker2022@gmail.com>
2024-02-01 22:33:55 -0500

    feat(clox): impl more value types

--- src/value.c
+++ src/value.c
@@ -11,6 +11,21 @@
 extern "C" {
 #endif
 
+bool valuesEqual(Value a, Value b) {
+    if (a.type != b.type) return false;
+    switch (a.type) {
+        case VAL_NIL:
+            return true;
+        case VAL_BOOL:
+            return AS_BOOL(a) == AS_BOOL(b);
+        case VAL_NUMBER:
+            return AS_NUMBER(a) == AS_NUMBER(b);
+        default:
+            // Unreachable.
+            return false;
+    }
+}
+
 void initValueArray(ValueArray *array) {
     array->count = 0;
     array->capacity = 0;
@@ -34,7 +49,17 @@ void freeValueArray(ValueArray *array) {
 }
 
 void printValue(Value value) {
-    printf("%g", value);
+    switch (value.type) {
+        case VAL_NIL:
+            printf("nil");
+            break;
+        case VAL_BOOL:
+            printf(AS_BOOL(value) ? "true" : "false");
+            break;
+        case VAL_NUMBER:
+            printf("%g", AS_NUMBER(value));
+            break;
+    }
 }
 
 #ifdef __cplusplus
